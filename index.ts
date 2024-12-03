73d495e13416a2a4425aeea6ae414961b361ca35
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-03 18:29:00 +0100

    feat: day one part one

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+const file = Bun.file("puzzle-input.txt");
+const puzzleInput = await file.text();
+const values = puzzleInput.trim().split(/\s+/);
+
+const [columnLeft, columnRight] = values
+  .reduce<[string[], string[]]>(
+    ([leftArr, rightArr], value, index) => {
+      if (index % 2 === 0) {
+        leftArr.push(value);
+      } else {
+        rightArr.push(value);
+      }
+      return [leftArr, rightArr];
+    },
+    [[], []]
+  )
+  .map(column => column.sort());
+
+const totalDistance = columnLeft.reduce((total, leftValue, i) => {
+  return total + Math.abs(Number(leftValue) - Number(columnRight[i]));
+}, 0);
+
+console.log(totalDistance); // 1319616
