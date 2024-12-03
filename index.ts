b4c60da215e022126711b79236c453131e672583
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-03 19:37:15 +0100

    feat: day two part one

--- index.ts
+++ index.ts
@@ -1,56 +1,30 @@
 const file = Bun.file("puzzle-input.txt");
 const puzzleInput = await file.text();
-const values = puzzleInput.trim().split(/\s+/);
+const reports = puzzleInput.trim().split("\n").map(line => line.split(/\s+/).map(Number));
 
-const [columnLeft, columnRight] = values
-  .reduce<[string[], string[]]>(
-    ([leftArr, rightArr], value, index) => {
-      if (index % 2 === 0) {
-        leftArr.push(value);
-      } else {
-        rightArr.push(value);
-      }
-      return [leftArr, rightArr];
-    },
-    [[], []]
-  )
-  .map(column => column.sort().map(Number));
+const safeReports = reports.filter((levels) => {
+  let status: 'safeIncrease' | 'safeDecrease' | 'unsafe' | 'none' = 'none';
 
-const totalDistance = columnLeft.reduce((total, leftValue, i) => {
-  return total + Math.abs(leftValue - columnRight[i]);
-}, 0);
+  for (let index = 0; index < levels.length - 1; index++) {
+    const absoluteDiff = Math.abs(levels[index + 1] - levels[index]);
+    const isSafeDiff = absoluteDiff <= 3 && absoluteDiff > 0;
 
-console.log(totalDistance); // 1319616
-
-const similarityScore = columnLeft.reduce((total, leftValue, i) => {
-  let cursor = i;
-  let multiplicator = 0;
-  const firstObservedValue = columnRight[i];
-
-  if (firstObservedValue > leftValue) {
-    while(columnRight[cursor] !== undefined && columnRight[cursor] >= leftValue) {
-      if (columnRight[cursor] === leftValue) {
-        multiplicator++;
-      }
-      cursor--;
-    }
-  } else {
-    if (firstObservedValue === leftValue) {
-      // Adjust the cursor to the lowest index of the same value
-      while(columnRight[cursor] !== undefined && columnRight[cursor] === leftValue) {
-        cursor--;
-      }
+    if (!isSafeDiff) {
+      status = 'unsafe';
+      break;
     }
 
-    while(columnRight[cursor] !== undefined && columnRight[cursor] <= leftValue) {
-      if (columnRight[cursor] === leftValue) {
-        multiplicator++;
-      }
-      cursor++;
+    const nextStatus = levels[index + 1] > levels[index] ? 'safeIncrease' : 'safeDecrease';
+
+    if (status === 'none') {
+      status = nextStatus;
+    } else if (status !== nextStatus) {
+      status = 'unsafe';
+      break;
     }
   }
 
-  return total + (leftValue * multiplicator);
-}, 0);
+  return status === 'safeIncrease' || status === 'safeDecrease';
+});
 
-console.log(similarityScore); // 27267728
+console.log(safeReports.length); // 591
