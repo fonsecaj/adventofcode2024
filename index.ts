2f6dd165a1cc62c5d3e59dc90062fa263a3cfb8c
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-04 18:41:29 +0100

    feat: day four part two

--- index.ts
+++ index.ts
@@ -3,70 +3,43 @@ const puzzleInput = await file.text();
 
 const charTable = puzzleInput.split("\n");
 
-const xmas = 'XMAS';
+const xmasChunk = 'MAS';
 
-function isXmas(charLookupFn: (i: number) => string) {
-  return Array.from({ length: xmas.length }, (_, i) => {
-    try {
-      return charLookupFn(i);
-    } catch (e) {
-      return '';
-    }
-  }).join("") === xmas;
+function isXmas(wordBuilder: () => [string, string, string]) {
+  try {
+    const word = wordBuilder().join("");
+
+    return word === xmasChunk || word === xmasChunk.split("").reverse().join("");
+  } catch (e) {
+    return false;
+  }
 }
 
-function checkXmasTop(column: number, row: number) {
-  return isXmas(i => charTable[row + i][column]);
+function checkXmasDiagonally(column: number, row: number) {
+  return isXmas(() => ([
+    charTable[row - 1][column - 1],
+    charTable[row][column],
+    charTable[row + 1][column + 1]
+  ]));
 }
 
-function checkXmasBottom(column: number, row: number) {
-  return isXmas(i => charTable[row - i][column]);
-}
-
-function checkXmasRight(column: number, row: number) {
-  return isXmas(i => charTable[row][column + i]);
-}
-
-function checkXmasLeft(column: number, row: number) {
-  return isXmas(i => charTable[row][column - i]);
-}
-
-function checkXmasBottomRight(column: number, row: number) {
-  return isXmas(i => charTable[row + i][column + i]);
-}
-
-function checkXmasBottomLeft(column: number, row: number) {
-  return isXmas(i => charTable[row + i][column - i]);
-}
-
-function checkXmasTopRight(column: number, row: number) {
-  return isXmas(i => charTable[row - i][column + i]);
-}
-
-function checkXmasTopLeft(column: number, row: number) {
-  return isXmas(i => charTable[row - i][column - i]);
+function checkXmasDiagonallyReverse(column: number, row: number) {
+  return isXmas(() => ([
+    charTable[row + 1][column - 1],
+    charTable[row][column],
+    charTable[row - 1][column + 1]
+  ]));
 }
 
 const xmasCount = charTable.reduce((count, row, rowIndex) => {
   let matches = 0;
   for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
-    if (row[columnIndex] === xmas[0]) {
-      const checks = [
-        checkXmasTop(columnIndex, rowIndex),
-        checkXmasBottom(columnIndex, rowIndex),
-        checkXmasRight(columnIndex, rowIndex),
-        checkXmasLeft(columnIndex, rowIndex),
-        checkXmasBottomRight(columnIndex, rowIndex),
-        checkXmasBottomLeft(columnIndex, rowIndex),
-        checkXmasTopRight(columnIndex, rowIndex),
-        checkXmasTopLeft(columnIndex, rowIndex),
-      ].filter(Boolean);
-
-      matches += checks.length;
+    if (row[columnIndex] === xmasChunk[1] && checkXmasDiagonally(columnIndex, rowIndex) && checkXmasDiagonallyReverse(columnIndex, rowIndex)) {
+      matches++;
     }
   }
 
   return count + matches;
 }, 0);
 
-console.log(xmasCount); // 2639
+console.log(xmasCount); // 2005
