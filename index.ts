020c2f83e5df742b3bcc991cc9069cb9a43656c7
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-04 12:26:05 +0100

    feat: day three part two

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
 const file = Bun.file("puzzle-input.txt");
 const corruptedMemory = await file.text();
 
-const mulRegex = /mul\((\d{1,3}),(\d{1,3})\)/gm;
+const mulRegex = /(do|don't)\(\)|mul\((\d{1,3}),(\d{1,3})\)/gm;
 
 let result;
 let sum = 0;
+let ignore = false;
 
 while ((result = mulRegex.exec(corruptedMemory)) !== null) {
-  const [_, a, b] = result;
-  sum = sum + (parseInt(a) * parseInt(b));
+  const [_, instruction, a, b] = result;
+
+  if (instruction) {
+    ignore = instruction === "don't";
+    continue;
+  }
+
+  if (!ignore) {
+    sum = sum + (parseInt(a) * parseInt(b));
+  }
 }
 
-console.log(sum); // 179571322
+console.log(sum); // 103811193
