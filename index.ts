fc36a112f45f71fcbe2baf1ad8ab88b206caf569
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-04 11:25:45 +0100

    feat: day three part one

--- index.ts
+++ index.ts
@@ -1,60 +1,14 @@
 const file = Bun.file("puzzle-input.txt");
-const puzzleInput = await file.text();
-const reports = puzzleInput.trim().split("\n").map(line => line.split(/\s+/).map(Number));
+const corruptedMemory = await file.text();
 
-const computeStatus = (levels: number[], tolerateBadLevel = false): 'safeIncrease' | 'safeDecrease' | 'unsafe' => {
-  let status: 'safeIncrease' | 'safeDecrease' | 'unsafe' | 'none' = 'none';
+const mulRegex = /mul\((\d{1,3}),(\d{1,3})\)/gm;
 
-  for (let index = 0; index < levels.length - 1; index++) {
-    const absoluteDiff = Math.abs(levels[index + 1] - levels[index]);
-    const isSafeDiff = absoluteDiff <= 3 && absoluteDiff > 0;
+let result;
+let sum = 0;
 
-    if (!isSafeDiff) {
-      status = 'unsafe';
-    }
-
-    const nextStatus = levels[index + 1] > levels[index] ? 'safeIncrease' : 'safeDecrease';
-
-    if (status === 'none') {
-      status = nextStatus;
-    } else if (status !== nextStatus) {
-      status = 'unsafe';
-    }
-
-    if (status === 'unsafe') {
-      if (tolerateBadLevel) {
-        let removedLevelIndex = 0;
-        while (removedLevelIndex < levels.length) {
-          const newLevels = [...levels];
-          newLevels.splice(removedLevelIndex, 1);
-
-          status = computeStatus(newLevels);
-
-          if (status !== 'unsafe') {
-            break;
-          }
-
-          removedLevelIndex++;
-        }
-      }
-
-      break;
-    }
-  }
-
-  if (status === 'none') {
-    throw new Error('Invalid status');
-  }
-
-  return status;
+while ((result = mulRegex.exec(corruptedMemory)) !== null) {
+  const [_, a, b] = result;
+  sum = sum + (parseInt(a) * parseInt(b));
 }
 
-const safeReports = reports.filter((levels) => {
-  let status: 'safeIncrease' | 'safeDecrease' | 'unsafe' | 'none' = 'none';
-
-  status = computeStatus(levels, true);
-
-  return status === 'safeIncrease' || status === 'safeDecrease';
-});
-
-console.log(safeReports.length); // 621
+console.log(sum); // 179571322
