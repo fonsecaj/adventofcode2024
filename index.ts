582e827266256c2cfe76e506632fb2ee901ed550
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-18 17:38:18 +0100

    feat: day eight part one

--- index.ts
+++ index.ts
@@ -1,64 +1,75 @@
 const file = Bun.file("puzzle-input.txt");
 const puzzleInput = await file.text();
-const equationLines = puzzleInput.split('\n');
+const antennasMap = puzzleInput.split('\n').map(row => row.split(''));
+const antennasMapRowCount = antennasMap.length;
+const antennasMapColCount = antennasMap[0].length;
+const antinodeMap = puzzleInput.replace(/[0-9a-zA-Z]/g, '.').split('\n').map(row => row.split(''));
 
-type Operator = '+' | '*' | '|';
-
-function generateOperatorCombinations(operators: Operator[], length: number): Operator[][] {
-  const combinationsCount = Math.pow(operators.length, length);
-
-  return Array.from({ length: combinationsCount }, (_, i) => {
-    return i
-      .toString(operators.length)
-      .padStart(length, '0')
-      .split('')
-      .map(digit => operators[Number(digit)]);
-  });
+type AntennaPosition = {
+  x: number;
+  y: number;
+  value: string;
 }
 
-function evaluateEquation(operators: Operator[], numbers: number[]): number {
-  let result = numbers[0];
-  
-  for (let i = 1; i < numbers.length; i++) {
-      const currentOperator = operators[i - 1];
-      const currentNumber = numbers[i];
-      
-      switch (currentOperator) {
-          case '+':
-              result += currentNumber;
-              break;
-          case '|':
-              result = Number(`${result}${currentNumber}`);
-              break;
-          case '*':
-              result *= currentNumber;
-              break;
+function writeAntinodes(antenna: string) {
+  console.log(`Writing antinodes for ${antenna}...\n`);
+
+  if (antenna.length > 1 || antenna === '.') return;
+
+  const antennaPositions = antennasMap.reduce((acc, row, y) => {
+    const antennaPosition = row.map((value, x) => {
+      if (value === antenna) {
+        return { x, y, value };
       }
-  }
-  
-  return result;
-}
+    }).filter((value): value is AntennaPosition => value !== undefined);
 
-function sumValidEquations(operators: Operator[]): number {
-  let totalSum = 0;
+    return [...acc, ...antennaPosition];
+  }, [] as AntennaPosition[]);
 
-  for (let lineIndex = 0; lineIndex < equationLines.length; lineIndex++) {
-    const [targetString, ...numberStrings] = equationLines[lineIndex].replace(':', '').split(' ');
-    const targetResult = Number(targetString);
-    const numbers = numberStrings.map(Number);
+  for (let i = 0; i < antennaPositions.length; i++) {
+    const { x: ax1, y: ay1 } = antennaPositions[i];
+    
+    for (let j = i + 1; j < antennaPositions.length; j++) {
+      const { x: ax2, y: ay2 } = antennaPositions[j];
+      const axDiff = ax2 - ax1;
+      const ayDiff = ay2 - ay1;
 
-    const operatorCombinations = generateOperatorCombinations(operators, numbers.length - 1);
+      const nx1 = ax1 + (axDiff * -1);
+      const ny1 = ay1 + (ayDiff * -1);
 
-    for (const operatorCombination of operatorCombinations) {
-      if (evaluateEquation(operatorCombination, numbers) === targetResult) {
-          console.log(`[${lineIndex}/${equationLines.length}] ✅\n`);
-          totalSum += targetResult;
-          break;
+      if (nx1 >= 0 && nx1 < antennasMapColCount && ny1 >= 0 && ny1 < antennasMapRowCount) {
+        antinodeMap[ny1][nx1] = '#';
+      }
+
+      const nx2 = ax2 - (axDiff * -1);
+      const ny2 = ay2 - (ayDiff * -1);
+
+      if (nx2 >= 0 && nx2 < antennasMapColCount && ny2 >= 0 && ny2 < antennasMapRowCount) {
+        antinodeMap[ny2][nx2] = '#';
       }
     }
   }
-
-  return totalSum;
 }
 
-console.log(sumValidEquations(['+', '*', '|'])); // 426214131924213
+const scannedAntennas = new Set<string>();
+
+for (let y = 0; y < antennasMapRowCount; y++) {
+  for (let x = 0; x < antennasMapColCount; x++) {
+    const antennaOrBlank = antennasMap[y][x];
+
+    if (antennaOrBlank === '.' || scannedAntennas.has(antennaOrBlank)) continue;
+    
+    writeAntinodes(antennaOrBlank);
+    scannedAntennas.add(antennaOrBlank);
+  }
+}
+
+console.log(antennasMap.map(row => row.join('')).join('\n'));
+console.log('\n')
+console.log(antinodeMap.map(row => row.join('')).join('\n'));
+
+const antinodeCount = antinodeMap.reduce((acc, row) => {
+  return acc + row.filter(value => value === '#').length;
+}, 0);
+
+console.log(`\nAntinode count: ${antinodeCount}`); // 254
