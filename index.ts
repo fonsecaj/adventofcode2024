11b150ad5d043a8ca87e5ad1be0cab9c3999b995
Jordan Fonseca <jordan.fonseca@agicap.com>
2024-12-18 23:04:41 +0100

    feat: day ten part one

--- index.ts
+++ index.ts
@@ -1,137 +1,46 @@
 const file = Bun.file("puzzle-input.txt");
 const puzzleInput = await file.text();
 
-type FileBlock = number | null;
+type TrailLevel = 0|1|2|3|4|5|6|7|8|9;
+type TrailMap = TrailLevel[][];
 
-function createFileSystem(diskMap: string): FileBlock[] {
-  const blocks: FileBlock[] = [];
-  let isFileSection = true;
-  let fileId = 0;
-    
-  const sections = diskMap.trim().split('').map(Number);
-  
-  for (const sectionSize of sections) {
-    if (isFileSection) {
-      blocks.push(...Array(sectionSize).fill(fileId));
-      fileId++;
-    } else {
-      blocks.push(...Array(sectionSize).fill(null));
-    }
-    isFileSection = !isFileSection;
+const trailMap = puzzleInput.split("\n").map((row) => row.trim().split("").map(Number)) as TrailMap;
+
+const trailUniquePaths = new Set<string>();
+
+console.log('\n\nStarting trail...\n');
+
+for (let y = 0; y < trailMap.length; y++) {
+  for (let x = 0; x < trailMap[y].length; x++) {
+    trail([[x, y]], 0);
   }
-  
-  return blocks;
 }
 
-function switchPositions(fileSystem: FileBlock[], positionA: number, positionB: number): void {
-  const temp = fileSystem[positionA];
-  fileSystem[positionA] = fileSystem[positionB];
-  fileSystem[positionB] = temp;
+function trail(path: [number, number][], expectedLevel: number): void {
+  const [x, y] = path[path.length - 1];
+
+  try {
+    if (trailMap[y][x] !== expectedLevel) {
+      return;
+    } 
+  } catch (e) {
+    return;
+  }
+  
+  if (expectedLevel === 9) {
+    const sizeBeforeAdd = trailUniquePaths.size;
+    trailUniquePaths.add(`${path[0].join(",")}:${x},${y}`);
+    if (trailUniquePaths.size !== sizeBeforeAdd) {
+      console.log(path.join(" ➡️ "), '🥾\n');
+    }
+    return;
+  }
+
+  trail([...path, [x - 1, y]], expectedLevel + 1);
+  trail([...path, [x + 1, y]], expectedLevel + 1);
+  trail([...path, [x, y - 1]], expectedLevel + 1);
+  trail([...path, [x, y + 1]], expectedLevel + 1);
 }
 
-function defragmentFileSystem(fileSystem: FileBlock[]): FileBlock[] {
-  const defragmented = [...fileSystem];
-  
-  let firstFreePositionStart = defragmented.findIndex(block => block === null);
-  let firstFreePositionEnd = firstFreePositionStart;
 
-  while (defragmented[firstFreePositionEnd] === null) {
-    firstFreePositionEnd++;
-  }
-
-  let currentPositionEnd = defragmented.length - 1;
-  
-  while (defragmented[currentPositionEnd] === null) {
-    currentPositionEnd--;
-  }
-
-  let currentPositionStart = currentPositionEnd;
-
-  while (defragmented[currentPositionStart - 1] === defragmented[currentPositionEnd]) {
-    currentPositionStart--;
-  }
-
-  let currentFileId = defragmented[currentPositionStart];
-  
-  while (currentFileId) {
-    let switched = false;
-
-    while(firstFreePositionStart < currentPositionStart && !switched) {
-      if (firstFreePositionEnd - firstFreePositionStart >= currentPositionEnd - currentPositionStart) {
-        for (let i = 0; i <= currentPositionEnd - currentPositionStart; i++) {
-          switchPositions(defragmented, currentPositionStart + i, firstFreePositionStart + i);
-        }
-  
-        firstFreePositionStart = defragmented.findIndex(block => block === null);
-        firstFreePositionEnd = firstFreePositionStart;
-    
-        while (defragmented[firstFreePositionEnd] === null) {
-          firstFreePositionEnd++;
-        }
-  
-        currentPositionEnd = defragmented.length - 1;
-  
-        while (defragmented[currentPositionEnd] === null) {
-          currentPositionEnd--;
-        }
-  
-        currentPositionStart = currentPositionEnd;
-  
-        while (defragmented[currentPositionStart - 1] === defragmented[currentPositionEnd]) {
-          currentPositionStart--;
-        }
-  
-        switched = true;
-      } else {
-        firstFreePositionStart = firstFreePositionEnd + 1;
-
-        while (defragmented[firstFreePositionStart] !== null && firstFreePositionStart < defragmented.length) {
-          firstFreePositionStart++;
-        }
-
-        firstFreePositionEnd = firstFreePositionStart;
-
-        while (defragmented[firstFreePositionEnd + 1] === null) {
-          firstFreePositionEnd++;
-        }
-      }
-    }
-
-    firstFreePositionStart = defragmented.findIndex(block => block === null);
-    firstFreePositionEnd = firstFreePositionStart;
-
-    while (defragmented[firstFreePositionEnd + 1] === null) {
-      firstFreePositionEnd++;
-    }
-
-    currentFileId = currentFileId ? currentFileId - 1 : 0;
-
-    currentPositionEnd = defragmented.length - 1;
-
-    while (defragmented[currentPositionEnd] !== currentFileId) {
-      currentPositionEnd--;
-    }
-
-    currentPositionStart = currentPositionEnd;
-
-    while (defragmented[currentPositionStart - 1] === currentFileId) {
-      currentPositionStart--;
-    }
-  }
-  
-  return defragmented;
-}
-
-function calculateChecksum(fileSystem: FileBlock[]): number {
-  return fileSystem.reduce((sum, block, position) => {
-    if (block !== null) {
-      return sum! + (position * block);
-    }
-    return sum ?? 0;
-  }, 0) as number;
-}
-
-const fileSystem = createFileSystem(puzzleInput);
-const defragmentedSystem = defragmentFileSystem(fileSystem);
-
-console.log(calculateChecksum(defragmentedSystem)); // 6361380647183
+console.log(trailUniquePaths.size); // 512
